c94923e5fa6988a041fffaa35b1ccfb1935abb58
Dalux C <daluxc1@gmail.com>
2022-03-23 16:49:47 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router/index'
+import App from './App.vue'
+
+Vue.use(ElementUI);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
